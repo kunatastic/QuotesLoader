3e731ebf2083b107c9cdfc8bf724a86e2f574469
kunatastic <kjha6666@gmail.com>
2020-12-11 13:25:19 +0530

    styling

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
 const search = async () => {
-  const data = await fetch("https://type.fit/api/quotes");
-  const quotes = await data.json();
+  try {
+    const data = await fetch("https://type.fit/api/quotes");
+    const quotes = await data.json();
+    localStorage.setItem("quotes", JSON.stringify(quotes));
+    extract(quotes);
+  } catch (e) {
+    document.getElementById("visible").style.display = "block";
+  }
+};
+function check() {
+  if (localStorage.getItem("quotes") == null) {
+    search();
+  } else {
+    var retrievedObject = localStorage.getItem("quotes");
+    var retrievedObject = JSON.parse(retrievedObject);
+    extract(retrievedObject);
+  }
+}
 
+function extract(quotes) {
   let length = quotes.length;
   let index = Math.floor(Math.random() * length);
-  let quote = await quotes[index];
+  let quote = quotes[index];
   process("author", "h2", "~" + quote.author);
   process("text", "h1", quote.text);
-};
+}
 
 function process(node, element, item) {
   const text = document.getElementById(node);
   var textNode = document.createElement(element);
   var textdata = document.createTextNode(item);
   textNode.appendChild(textdata);
-  console.log(textNode);
   text.appendChild(textNode);
 }
-
-search();
